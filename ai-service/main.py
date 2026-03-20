5fcf8e0e772a8bf5d2cd4e3e0fd9b5b394bb04ed
yabswannalearn <reinaelyabut1234@gmail.com>
2026-03-20 14:05:13 +0800

    add: cors to backend

--- ai-service/main.py
+++ ai-service/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import database_routes, generate_routes
 
 app = FastAPI()
+
+# Add this CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(database_routes.router)
 app.include_router(generate_routes.router)
